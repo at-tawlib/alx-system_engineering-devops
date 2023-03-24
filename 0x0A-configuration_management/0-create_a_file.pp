ceec4c8f3234974ed0dcddb6e053889596fef516
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-24 18:40:48 +0000

    Spell check of Puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,7 @@
 file { '/tmp/school':
   ensure  => 'file',
   path    => '/tmp/school',
-  content => 'I love puppet',
+  content => 'I love Puppet',
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
