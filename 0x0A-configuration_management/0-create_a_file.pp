a3a075a9ef13e18c51ae6d9a5ea97496f6492476
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-03-24 18:38:22 +0000

    Clean up

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # create file /tmp/school with permission=0744, owner=www-data, group=www-data and content='I love puppet'
-file { 'school':
-  ensure  => file,
+file { '/tmp/school':
+  ensure  => 'file',
   path    => '/tmp/school',
   content => 'I love puppet',
   owner   => 'www-data',
