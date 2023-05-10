a84fd8a1aa0ab58a6fba06256480310402a29145
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-05-10 04:15:52 +0000

    Create 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Recursive function that queries a Reddit
+"""
+import requests
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    recursive functions the queries teh reddit and returns a
+    list containing the titles of all hot articles of a subreddit
+    """
+    global after
+
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    agent = {'User-agent': 'Google Chrome Version 113.0.5672.63'}
+    response = requests.get(url, headers=agent, params={'after': after},
+                            allow_redirects=False)
+
+    if results.status_code == 200:
+        after_results = results.json().get('data').get('after')
+        if after_results is not None:
+            after = after_results
+            recurse(subreddit, hot_list)
+        titles = results.json().get('data').get('children')
+        for title in titles:
+            hot_list.append(title.get('data').get('title')
+        return hot_list
+    else:
+        return None
