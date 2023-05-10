a456c85fe5a9076c5adb613276f0ec048dc75b10
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-05-10 03:37:52 +0000

    Create 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API and returns the number of subscribers
+    for a given subreddit
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    response = requests.get(url, headers=agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+    except Exception:
+        return 0
