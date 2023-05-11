125b4dcffaf02b336db814d4d0dc7790295cad0c
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-05-11 02:10:17 +0000

    Change phppp to phpp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,7 @@
 # there is a spelling error i.e. phpp
 # so we change phpp to php
 $edit = '/var/www/html/wp-settings.php'
-exec {'change':
-  command => "sed -i 's/phppp/php/g' ${edit}",
+exec { 'change':
+  command => "sed -i 's/phpp/php/g' ${edit}",
   path    => ['/bin', '/usr/bin']
 }
